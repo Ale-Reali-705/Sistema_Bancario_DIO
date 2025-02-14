601c5d4a3fc1d3c344476616adec7c7c62b45b52
Ale-Reali-705 <alessandro.reali.3110@gmail.com>
2025-02-14 13:24:51 -0300

    Estrutura do codigo

--- Sistema_Bancario.py
+++ Sistema_Bancario.py
@@ -1,1 +1,19 @@
-# testando sincronia
+'''
+Sistema Bancario: depósito, saque, extrato
+
+depósito:
+    apenas valores positivos
+    armazenar o valor depositado na variavel extrato
+
+saque:
+    limite diário de 3 saques
+    500 reais por saque
+    em caso de saldo insuficiente, informar ao usuário
+    armazenar o valor sacado na variavel extrato
+
+extrato:
+    listar todos os depósitos e saques realizados
+    se não houver movimentações, informar ao usuário "Não foram realizadas movimentações."
+    formato: "Depósito: R$ 1000.00"
+'''
+
